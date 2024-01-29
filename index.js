cb42be1c3e7d414b43ef50f32f6b92e352b82b54
Myrhiam Rodriguez <myrhiamrodriguez@Myrhiams-MacBook-Air.local>
2024-01-28 18:04:03 -0800

    homework - class 2

--- index.js
+++ index.js
@@ -1,6 +1,40 @@
-let greenCharacter = document.createElement('img')
-greenCharacter.src = 'assets/green-character.gif'
-greenCharacter.style.position = 'fixed'
-greenCharacter.style.left = '100px'
-greenCharacter.style.bottom = '100px'
-document.body.append(greenCharacter)
+function newImage(url, left, bottom) {
+    let object = document.createElement('img');
+    object.src = url;
+    object.style.position = 'fixed';
+    object.style.left = left + 'px';
+    object.style.bottom = bottom + 'px';
+    document.body.append(object);
+    return object;
+}
+
+function newItem(url, left, bottom) {
+    let object = newImage(url, left, bottom);
+
+    object.addEventListener('dblclick', () => {
+        object.remove();
+    });
+}
+
+// Using newImage function to add images to the screen
+newImage('assets/green-character.gif', 100, 100);
+newImage('assets/tree.png', 200, 300);
+newImage('assets/pillar.png', 350, 100);
+newImage('assets/pine-tree.png', 450, 200);
+newImage('assets/crate.png', 150, 200);
+newImage('assets/well.png', 500, 425);
+
+// Using newItem function to add items to the screen
+newItem('assets/sword.png', 500, 405);
+newItem('assets/shield.png', 165, 185);
+newItem('assets/staff.png', 600, 100);
+
+// Bonus: Using the tile function to add background images
+function tile(url, left, bottom, width, height) {
+    for (let h = 0; h < height; h++) {
+        for (let w = 0; w < width; w++) {
+            newImage(url, left + w * 100, bottom + h * 100);
+        }
+    }
+}
+
