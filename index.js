4502b43908f51093d5a0e02b6120d31340b49c6f
Myrhiam Rodriguez <myrhiamrodriguez@Myrhiams-MacBook-Air.local>
2024-02-02 18:09:52 -0800

    finished homework class 2 mr

--- index.js
+++ index.js
@@ -1,40 +1,32 @@
-function newImage(url, left, bottom) {
-    let object = document.createElement('img');
-    object.src = url;
-    object.style.position = 'fixed';
-    object.style.left = left + 'px';
-    object.style.bottom = bottom + 'px';
-    document.body.append(object);
-    return object;
+//fixed postion newImage
+function newImage(path, left, bottom) {
+    let blank = document.createElement('img');
+    blank.src = path;  
+    blank.style.position = 'fixed';  
+    blank.style.left = left + 'px';  
+    blank.style.bottom = bottom + 'px';
+    document.body.append(blank);
+    return blank;
 }
 
-function newItem(url, left, bottom) {
-    let object = newImage(url, left, bottom);
+//function to create a newItem
+function newItem(path, left, bottom) {
+    let element = newImage(path, left, bottom);
 
-    object.addEventListener('dblclick', () => {
-        object.remove();
+//remove element
+    element.addEventListener('dblclick', () => {
+        element.remove();
     });
 }
 
-// Using newImage function to add images to the screen
-newImage('assets/green-character.gif', 100, 100);
-newImage('assets/tree.png', 200, 300);
-newImage('assets/pillar.png', 350, 100);
-newImage('assets/pine-tree.png', 450, 200);
-newImage('assets/crate.png', 150, 200);
-newImage('assets/well.png', 500, 425);
 
-// Using newItem function to add items to the screen
+// creating newItems
+newItem('assets/green-character.gif', 100, 100);
+newItem('assets/well.png', 500,425);
+newItem('assets/tree.png', 200,300);
 newItem('assets/sword.png', 500, 405);
-newItem('assets/shield.png', 165, 185);
 newItem('assets/staff.png', 600, 100);
-
-// Bonus: Using the tile function to add background images
-function tile(url, left, bottom, width, height) {
-    for (let h = 0; h < height; h++) {
-        for (let w = 0; w < width; w++) {
-            newImage(url, left + w * 100, bottom + h * 100);
-        }
-    }
-}
-
+newItem('assets/shield.png', 165, 185);
+newItem('assets/pine-tree.png',450,200);
+newItem('assets/pillar.png', 350, 100);
+newItem('assets/crate.png', 150, 200);
